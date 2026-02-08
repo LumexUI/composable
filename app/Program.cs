c473361d7a672caca31f48830db30af785b2c740
desmondinho <daniil.norinn@gmail.com>
2026-02-08 23:20:02 +0200

    add small demos

--- app/Program.cs
+++ app/Program.cs
@@ -2,10 +2,14 @@ using LumexUI.Composable;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using TailwindMerge.Extensions;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTailwindMerge();
 
 await builder.Build().RunAsync();
